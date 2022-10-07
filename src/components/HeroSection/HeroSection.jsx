33a6cc339bf377d20df8f05bc5a54bb5de4d8d98
NomanKhan3701 <noman.khan3701@gmail.com>
2022-10-07 11:45:24 +0530

    first commit

---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './HeroSection.scss'
+
+const HeroSection = () => {
+  return (
+    <div>HeroSection</div>
+  )
+}
+
+export default HeroSection
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import '/public/assets/css/globals.scss'
+import '/public/assets/css/utils.scss'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
